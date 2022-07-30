0c5345cebd4d675ca82ec854b2b2325682108ff0
adithya <madithyam16@gmail.com>
2022-07-30 13:39:38 +0530

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        welcome To <br />
+        my personal portfolio
+      </SectionTitle>
+      <SectionText>
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going through
+        the cites of the word in classical literatur
+      </SectionText>
+      <Button onclick={() => (window.location = 'https://google.com')}>
+        Learn more
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
